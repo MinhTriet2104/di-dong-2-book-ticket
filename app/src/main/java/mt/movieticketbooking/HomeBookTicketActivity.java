fcd9e647b2c22f5878c1ba3552d95bed1e6803a7
xuanvan213 <50666377+xuanvan213@users.noreply.github.com>
2020-07-21 20:51:42 +0700

    Add events to elements in the homepage

---
+++ app/src/main/java/mt/movieticketbooking/HomeBookTicketActivity.java
@@ -0,0 +1,88 @@
+package mt.movieticketbooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class HomeBookTicketActivity extends AppCompatActivity {
+
+    private Button btnBuyTicket;
+    private ImageView imageMovie;
+    private EditText edtRate;
+    private EditText edtYearsOld;
+    private EditText edtMoviesType;
+    private EditText edtSC;
+    Intent intent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_book_ticket_layout);
+        btnBuyTicket = findViewById(R.id.btnBuyTicket);
+        imageMovie = findViewById(R.id.imgMovies);
+        edtRate = findViewById(R.id.edtRate);
+        edtMoviesType = findViewById(R.id.movieType);
+        edtSC = findViewById(R.id.edtSC);
+        edtYearsOld = findViewById(R.id.edtYearsOld);
+        intent = getIntent();
+
+        // Set Event for btn Buy ticket
+        btnBuyTicket.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent.setClass(HomeBookTicketActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(intent);
+            }
+        });
+        // Set Event for movie image clicked
+        imageMovie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent.setClass(HomeBookTicketActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(intent);
+            }
+        });
+
+        // Set Event for movie image clicked
+        edtRate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String edtText = edtRate.getText() + "";
+                Toast.makeText(HomeBookTicketActivity.this, edtText , Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Set Event for movie edt clicked
+        edtYearsOld.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String edtText = edtYearsOld.getText() + "";
+                Toast.makeText(HomeBookTicketActivity.this, edtText , Toast.LENGTH_SHORT).show();
+            }
+        });
+        // Set Event for edt clicked
+        edtMoviesType.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String edtText = edtMoviesType.getText() + "";
+                Toast.makeText(HomeBookTicketActivity.this, edtText , Toast.LENGTH_SHORT).show();
+            }
+        });
+        // Set Event for edt clicked
+        edtSC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String edtText = edtSC.getText() + "";
+                Toast.makeText(HomeBookTicketActivity.this, edtText , Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
--- app/src/main/java/mt/movieticketbooking/MainActivity.java
+++ app/src/main/java/mt/movieticketbooking/MainActivity.java
@@ -2,14 +2,32 @@ package mt.movieticketbooking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Triet layout
-        setContentView(R.layout.home_book_ticket_layout);
+        setContentView(R.layout.book_ticket_layout);
+        final Intent intent = getIntent();
+
+        //get ID from layout
+        btnBack = findViewById(R.id.btnBack);
+
+        //Set Event for btn Back
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent.setClass(MainActivity.this, HomeBookTicketActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(intent);
+            }
+        });
     }
 }
