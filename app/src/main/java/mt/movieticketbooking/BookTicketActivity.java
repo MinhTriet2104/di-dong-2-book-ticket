30bec2aa849e8462c9827a8781d9f0c43a085897
Nguyễn Minh Triết <minhtriet2104.it@gmail.com>
2020-07-23 21:28:39 +0700

    create model Movie, get data from firestore for BookTicketActivity by Minh Triet

--- app/src/main/java/mt/movieticketbooking/BookTicketActivity.java
+++ app/src/main/java/mt/movieticketbooking/BookTicketActivity.java
@@ -1,17 +1,28 @@
 package mt.movieticketbooking;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
 public class BookTicketActivity extends AppCompatActivity {
 
+    private DocumentReference nDocRefMovie = FirebaseFirestore.getInstance().document("book-ticket/movie");
     private Button btnBack;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +40,15 @@ public class BookTicketActivity extends AppCompatActivity {
                 intent.setClass(BookTicketActivity.this, HomeBookTicketActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
                 startActivity(intent);
+            }
+        });
+
+        nDocRefMovie.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    Log.d("Test", task.getResult().getData().toString());
+                }
             }
         });
     }
---
+++ app/src/main/java/mt/movieticketbooking/models/Movie.java
@@ -0,0 +1,12 @@
+package mt.movieticketbooking.models;
+
+import java.util.Date;
+
+public class Movie {
+    private String title;
+    private int duration;
+    private int rating;
+    private Date dateStart;
+    private Date dateEnd;
+    private String imageUrl;
+}
